40ee6ecd4aae369d4318f4fbf72ae6be5d47cb6d
Tiam <3036293856@qq.com>
2023-06-21 14:50:24 +0800

    修改发布说说时的表情包, 以unicode字符代替图片

--- blog-vue/admin/src/views/talk/Talk.vue
+++ blog-vue/admin/src/views/talk/Talk.vue
@@ -3,97 +3,54 @@
     <div class="title">{{ this.$route.name }}</div>
     <div class="talk-container">
       <!-- 输入框 -->
-      <Editor
-        ref="editor"
-        class="editor-wrapper"
-        v-model="talk.content"
-        placeholder="说点什么吧"
-      />
+      <Editor ref="editor" class="editor-wrapper" v-model="talk.content" placeholder="说点什么吧" />
       <!-- 操作菜单 -->
       <div class="operation-wrapper">
         <div class="left-wrapper">
           <!-- 表情 -->
           <el-popover placement="bottom-start" width="460" trigger="click">
-            <span
-              class="emoji-item"
-              v-for="(value, key, index) of emojiList"
-              :key="index"
-              @click="addEmoji(key, value)"
-            >
-              <img
+            <span class="emoji-item" v-for="(value, key, index) of emojiList" :key="index" @click="addEmoji(key, value)">
+              <!-- <img
                 :src="value"
                 :title="key"
                 class="emoji"
                 width="24"
                 height="24"
-              />
+              /> -->
+              <span class="emoji" :title="key">{{ value }}</span>
             </span>
-            <i
-              class="iconfont el-icon-mybiaoqing operation-btn"
-              slot="reference"
-            />
+            <i class="iconfont el-icon-mybiaoqing operation-btn" slot="reference" />
           </el-popover>
           <!-- 图片上传 -->
-          <el-upload
-            action="/api/admin/talks/images"
-            multiple
-            :before-upload="beforeUpload"
-            :on-success="upload"
-            :show-file-list="false"
-          >
+          <el-upload action="/api/admin/talks/images" multiple :before-upload="beforeUpload" :on-success="upload"
+            :show-file-list="false">
             <i class="iconfont el-icon-mytupian operation-btn" />
           </el-upload>
         </div>
         <div class="right-wrapper">
           <!-- 是否置顶 -->
-          <el-switch
-            style="margin-right:16px"
-            v-model="talk.isTop"
-            inactive-text="置顶"
-            :active-value="1"
-            :inactive-value="0"
-          />
+          <el-switch style="margin-right:16px" v-model="talk.isTop" inactive-text="置顶" :active-value="1"
+            :inactive-value="0" />
           <!-- 说说状态 -->
-          <el-dropdown
-            trigger="click"
-            @command="handleCommand"
-            style="margin-right:16px"
-          >
+          <el-dropdown trigger="click" @command="handleCommand" style="margin-right:16px">
             <span class="talk-status">
               {{ dropdownTitle }}<i class="el-icon-arrow-down el-icon--right" />
             </span>
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item
-                v-for="(item, index) of statusList"
-                :key="index"
-                :command="item.status"
-              >
+              <el-dropdown-item v-for="(item, index) of statusList" :key="index" :command="item.status">
                 {{ item.desc }}
               </el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
-          <el-button
-            type="primary"
-            size="small"
-            @click="saveOrUpdateTalk"
-            :disabled="talk.content == ''"
-          >
+          <el-button type="primary" size="small" @click="saveOrUpdateTalk" :disabled="talk.content == ''">
             发布
           </el-button>
         </div>
       </div>
       <!-- 图片上传 -->
-      <el-upload
-        class="talk-image-upload"
-        v-show="uploadList.length > 0"
-        action="/api/admin/talks/images"
-        list-type="picture-card"
-        :file-list="uploadList"
-        multiple
-        :before-upload="beforeUpload"
-        :on-success="upload"
-        :on-remove="handleRemove"
-      >
+      <el-upload class="talk-image-upload" v-show="uploadList.length > 0" action="/api/admin/talks/images"
+        list-type="picture-card" :file-list="uploadList" multiple :before-upload="beforeUpload" :on-success="upload"
+        :on-remove="handleRemove">
         <i class="el-icon-plus" />
       </el-upload>
     </div>
@@ -102,7 +59,7 @@
 
 <script>
 import * as imageConversion from "image-conversion";
-import EmojiList from "../../assets/js/emoji";
+import EmojiList from "../../assets/js/emojiall";
 import Editor from "../../components/Editor.vue";
 export default {
   components: {
@@ -122,7 +79,7 @@ export default {
         });
     }
   },
-  data: function() {
+  data: function () {
     return {
       emojiList: EmojiList,
       talk: {
@@ -144,12 +101,15 @@ export default {
       this.talk.status = command;
     },
     addEmoji(key, value) {
+      // this.$refs.editor.addText(
+      //   "<img src= '" +
+      //     value +
+      //     "' width='24'height='24' alt=" +
+      //     key +
+      //     " style='margin: 0 1px;vertical-align: text-bottom'/>"
+      // );
       this.$refs.editor.addText(
-        "<img src= '" +
-          value +
-          "' width='24'height='24' alt=" +
-          key +
-          " style='margin: 0 1px;vertical-align: text-bottom'/>"
+        `<span class="emoji">${value}</span>`
       );
     },
     handleRemove(file) {
@@ -223,45 +183,54 @@ export default {
 .talk-container {
   margin-top: 40px;
 }
+
 .editor-wrapper {
   min-height: 150px;
 }
+
 .operation-wrapper {
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin-top: 16px;
 }
+
 .operation-btn {
   cursor: pointer;
   color: #838383;
   font-size: 20px;
   margin-right: 12px;
 }
+
 .talk-status {
   cursor: pointer;
   font-size: 12px;
   color: #999;
 }
+
 .emoji {
   user-select: none;
   margin: 0.25rem;
   display: inline-block;
   vertical-align: middle;
 }
+
 .emoji-item {
   cursor: pointer;
   display: inline-block;
 }
+
 .emoji-item:hover {
   transition: all 0.2s;
   border-radius: 0.25rem;
   background: #dddddd;
 }
+
 .left-wrapper {
   display: flex;
   width: 50%;
 }
+
 .talk-image-upload {
   margin-top: 8px;
 }
